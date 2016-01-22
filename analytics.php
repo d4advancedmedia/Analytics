83a2f99eab7e12289338c064189dce98cbc14080
GrayofD4 <grayson@d4webdesign.com>
2016-01-22 09:40:54 -0800

    Working copy, commented out login code by default

--- analytics.php
+++ analytics.php
@@ -16,23 +16,25 @@ function footer_analytics() {
 	$analytics_account = '';
 	$uacode = 'UA-XXXXX-X';
 
-	$output  = '<script>';
 
-		if ( is_user_logged_in() ) {
-			$output .= '// Acct: '. $analytics_acct;
-		}
 
-		$output .= "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');";
 
-		$output .= "ga('create', '".$uacode."'', 'auto');";
-		$output .= "ga('send', 'pageview');";
+?><script>
 
-	$output .= '</script>';
+<?php # if ( is_user_logged_in() ) : ?>
+	// Acct: <?php echo $analytics_account; ?>
+<?php # endif; ?>
 
-	echo $output;
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', '<?php echo $uacode; ?>', 'auto');
+	ga('send', 'pageview');
+
+</script><?php 
+
 
 } add_action( 'wp_footer', 'footer_analytics' );
 
