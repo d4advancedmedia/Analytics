5ac4e808468172fc1981a6493e7b9b88c9c1d3d9
GrayofD4 <grayson@d4webdesign.com>
2016-01-22 09:03:59 -0800

    Updated flexible output, added logged in acct show

--- analytics.php
+++ analytics.php
@@ -11,30 +11,29 @@
 	License: GPL2
 */
 
-
-
-$uacode = 'UA-XXXXX-X';
-
-
 function footer_analytics() {
-	global $uacode;
-	echo(
 
+	$analytics_account = '';
+	$uacode = 'UA-XXXXX-X';
 
-		'<script>'.
+	$output  = '<script>';
 
-			"(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');".
+		if ( is_user_logged_in() ) {
+			$output .= '// Acct: '. $analytics_acct;
+		}
 
-			"ga('create', '".$uacode."'', 'auto');".
-			"ga('send', 'pageview');".
+		$output .= "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');";
 
-		'</script>'
+		$output .= "ga('create', '".$uacode."'', 'auto');";
+		$output .= "ga('send', 'pageview');";
 
+	$output .= '</script>';
 
-	);
+	echo $output;
+
 } add_action( 'wp_footer', 'footer_analytics' );
 
 
